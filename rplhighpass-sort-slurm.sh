ea82c6efecfa4b68293e8f9fd6a8e58fcd87bd25
AWS ParallelCluster user <ec2-user@ip-10-0-1-191.ap-southeast-1.compute.internal>
2026-09-06 17:18:46 +0000

    Use separate conda environments for spike sorting

--- rplhighpass-sort-slurm.sh
+++ rplhighpass-sort-slurm.sh
@@ -13,6 +13,11 @@
 #SBATCH -e rplhighpass-sort-slurm.%N.%j.err
 
 # LOAD MODULES, INSERT CODE, AND RUN YOUR PROGRAMS HERE
+/data/miniconda3/bin/conda init
+source ~/.bashrc
+envarg=`/data/src/PyHipp/envlist.py`
+conda activate $envarg
+
 python -u -c "import PyHipp as pyh; \
 import time; \
 pyh.RPLHighPass(saveLevel=1); \
@@ -21,3 +26,6 @@ mountain_batch.mountain_batch(); \
 from PyHipp import export_mountain_cells; \
 export_mountain_cells.export_mountain_cells(); \
 print(time.localtime());"
+
+conda deactivate
+/data/src/PyHipp/envlist.py $envarg
