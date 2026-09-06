5cf3aec01d0d9087f5a11799e583a6195173f806
AWS ParallelCluster user <ec2-user@ip-10-0-1-191.ap-southeast-1.compute.internal>
2026-09-06 17:08:00 +0000

    Complete envlist environment management

--- envlist.py
+++ envlist.py
@@ -16,21 +16,28 @@ RESET_MODE = 2
 
 # get number of arguments
 nargs = len(sys.argv)
+
 if nargs > 3:
     print('Usage: envlist; envlist env; envlist envprefix nenvs')
-    exit
+    exit()
+
 elif nargs > 2:
     # creates or re-creates the list of environments
     pmode = RESET_MODE
+
     # get prefix for environment name
     envprefix = sys.argv[1]
+
     # get number of environments to create
-    nenvs = sys.argv[2]
+    nenvs = int(sys.argv[2])
+
 elif nargs > 1:
     # returns env to the list of environments
     pmode = WRITE_MODE
+
     # get name of environment to return
     env = sys.argv[1]
+
 else:
     # gets name of an environment to use
     pmode = READ_MODE
@@ -40,19 +47,24 @@ lock = FileLock(lock_path, timeout=time_out_secs)
 
 with lock:
     if pmode == RESET_MODE:
-        # create a list (named clist) of nevns environments with the 
-        # prefix envprefix
-        # add code here
+        # create a list of nenvs environments with the prefix envprefix
+        clist = []
+
+        for x in range(nenvs):
+            clist.append(envprefix + str(x))
+
     else:
         # load hickle file
         clist = hickle.load(file_path)
 
         if pmode == WRITE_MODE:
             # append item to end of list
-            # add code here
-        else:    
+            clist.append(env)
+
+        else:
             # get and remove env from clist
-            # add code here
+            env = clist.pop(0)
+
             # return env name
             print(env)
 
